b6dfd06c2abe1c9e87d3ba9c02c8b24a1a802bb4
St Michael Egenamba <michaelegenamba@gmail.com>
2026-05-05 15:09:55 -0700

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
-import ProductList from './components/ProductList';
+import React, { useState } from "react";
+import ProductList from "./components/ProductList";
 
 const App = () => {
   // TODO: Define initial product data
+
+  const products = [
+    { id: 1, name: "Laptop", price: 1200, inStock: true },
+    { id: 2, name: "Iphone", price: 1000, inStock: true },
+    { id: 3, name: "Samsung", price: 900, inStock: true },
+    { id: 4, name: "Tablet", price: 400, inStock: true },
+  ];
 
   // TODO: Implement state to manage filtering
 
@@ -10,12 +17,12 @@ const App = () => {
 
   return (
     <div>
-      <h1>{/* TODO: Add 'Product Dashboard' title here */}</h1>
-      
+      <h1>Product Dashboard</h1>
+
       {/* TODO: Add buttons to allow filtering by availability */}
 
       {/* TODO: Render the ProductList component and pass filtered products */}
-      
+      <ProductList products={products} />
     </div>
   );
 };
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import styles from '../styles/ProductCard.module.css';
+import React from "react";
+import styles from "../styles/ProductCard.module.css";
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ name, price, stock }) => {
   return (
-    <div className>
-      {/* TODO: Apply conditional class to <div> above for out-of-stock items */}
-      
+    // TODO: Apply conditional class to <div> above for out-of-stock items
+    <div className={stock ? styles.card : styles.outOfStock}>
       {/* TODO: Display product name */}
+      <h3>{name}</h3>
 
       {/* TODO: Display product price */}
+      <p>{price}</p>
 
       {/* TODO: Show if the product is in stock or out of stock */}
-      
+      <p>{stock ? "In Stock" : "Out of Stock"}</p>
     </div>
   );
 };
